986984d005c1b968c5f4a64b68ba324ba89244ac
ferejbo <felix.rej.bottolfsen@gmail.com>
2022-11-21 01:12:45 +0100

    Task 2 maybe done

--- src/main/scala/Bank.scala
+++ src/main/scala/Bank.scala
@@ -1,28 +1,63 @@
 class Bank(val allowedAttempts: Integer = 3) {
 
-    private val transactionsQueue: TransactionQueue = new TransactionQueue()
-    private val processedTransactions: TransactionQueue = new TransactionQueue()
+  private val transactionsQueue: TransactionQueue = new TransactionQueue()
+  private val processedTransactions: TransactionQueue = new TransactionQueue()
 
-    def addTransactionToQueue(from: Account, to: Account, amount: Double): Unit = ???
-                                                // TODO
-                                                // project task 2
-                                                // create a new transaction object and put it in the queue
-                                                // spawn a thread that calls processTransactions
+  // TODO
+  // project task 2
+  // create a new transaction object and put it in the queue
+  // spawn a thread that calls processTransactions
+  def addTransactionToQueue(
+      from: Account,
+      to: Account,
+      amount: Double
+  ): Unit = {
+    val transaction = new Transaction(
+      transactionsQueue,
+      processedTransactions,
+      from,
+      to,
+      amount,
+      allowedAttempts
+    )
+    transactionsQueue.push(transaction)
+    // new Thread(new Runnable {
+    //   def run() = {
+    //     processTransactions
+    //   }
+    // }).run()
 
-    private def processTransactions: Unit = ???
-                                                // TOO
-                                                // project task 2
-                                                // Function that pops a transaction from the queue
-                                                // and spawns a thread to execute the transaction.
-                                                // Finally do the appropriate thing, depending on whether
-                                                // the transaction succeeded or not
+  }
 
-    def addAccount(initialBalance: Double): Account = {
-        new Account(this, initialBalance)
+  // TODO
+  // project task 2
+  // Function that pops a transaction from the queue
+  // and spawns a thread to execute the transaction.
+  // Finally do the appropriate thing, depending on whether
+  // the transaction succeeded or not
+  private def processTransactions: Unit = {
+    transactionsQueue.synchronized {
+      val iterator = transactionsQueue.iterator
+      while (iterator.hasNext) {
+        val transaction = transactionsQueue.pop
+        transaction.run()
+        if (transaction.status == TransactionStatus.PENDING) {
+          transactionsQueue.push(transaction)
+          processTransactions
+        } else {
+          processedTransactions.push(transaction)
+        }
+      }
     }
 
-    def getProcessedTransactionsAsList: List[Transaction] = {
-        processedTransactions.iterator.toList
-    }
+  }
+
+  def addAccount(initialBalance: Double): Account = {
+    new Account(this, initialBalance)
+  }
+
+  def getProcessedTransactionsAsList: List[Transaction] = {
+    processedTransactions.iterator.toList
+  }
 
 }
